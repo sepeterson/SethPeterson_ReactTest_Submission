6023e0eda18ca86921e7a4ebc25134893272df1c
Seth Peterson <seth@sethpeterson.net>
2019-07-19 18:41:39 -0400

    do some data filtering

--- src/App.js
+++ src/App.js
@@ -13,35 +13,56 @@ class App extends Component {
     isLoading: false
   };
 
+  componentDidMount() {
+    this.fetchMembers(0);
+  }
+
   isOkay = statusCode => {
     let diff = 200 - statusCode;
-    return !((diff < 0) | (diff > 99));
+    return !(diff < 0 || diff > 99);
+  };
+
+  updatePage(page) {
+    if (page >= 0 && page < this.state.numberPages) {
+      this.fetchMembers(page);
+    }
+  }
+
+  filterResponse = member => {
+    const currentCongress = member.congresses[0];
+    return {
+      _id: member._id,
+      addresses: member.addresses,
+      party: currentCongress.partyAffiliations[0].name,
+      committeeAssignments: currentCongress.committeeAssignments,
+      stateCode: currentCongress.stateCode,
+      stateDistrict: currentCongress.stateDistrict,
+      subCommitteeAssignments: currentCongress.subCommitteeAssignments,
+      officialName: member.officialName,
+      oathOfOfficeDate: member.oathOfOfficeDate
+    };
   };
 
   fetchMembers(page) {
-    this.setState({ isLoading: true });
+    this.setState({ isLoading: true, errorText: "" });
     fetch(
-      `https://api.allorigins.win/get?url=${encodeURIComponent(
-        `https://clerkapi.azure-api.net/Members?key=${
-          config.apiKey
-        }&$skip=${this.state.perPage * page}`
-      )}`
+      `${config.corsProxy}/https://clerkapi.azure-api.net/Members?key=${
+        config.apiKey
+      }&$skip=${this.state.perPage *
+        page}&$orderby=sortName&$filter=active%20eq%20'yes'`
     )
       .then(response => {
         if (response.ok) {
           return response.json();
         }
-        throw new Error("Can't reach proxy server!");
+        throw new Error("Error with proxy server!");
       })
       .then(membersJson => {
-        console.log(membersJson);
-        let memberList = JSON.parse(membersJson.contents);
-        if (!this.isOkay(membersJson.status.http_code)) {
-          throw new Error("Something else broke!");
-        }
-        let { pagination } = memberList;
+        let { pagination } = membersJson;
         this.setState({
-          members: memberList.results,
+          members: membersJson.results.map(member =>
+            this.filterResponse(member)
+          ),
           perPage: pagination.per_page,
           numberPages: pagination.number_pages,
           page: pagination.page,
@@ -53,32 +74,29 @@ class App extends Component {
       );
   }
 
-  componentDidMount() {
-    this.fetchMembers(0);
-  }
-
   render() {
+    const { members, page, numberPages, errorText } = this.state;
     return (
       <div className="App">
         <div>
           <h3>Members</h3>
-          {this.state.errorText ? <h3>{this.state.errorText}</h3> : null}
+          {this.state.errorText && <h3>{errorText}</h3>}
           {this.state.isLoading ? (
-            <h3>It's loading!!</h3>
+            <span>loading...</span>
           ) : (
-            <MemberList members={this.state.members} />
+            <MemberList members={members} />
           )}
-          <button
-            className="pageUp"
-            onClick={() => this.fetchMembers(this.state.page + 1)}
-          >
-            page plus one
+          <button className="pageUp" onClick={() => this.updatePage(page - 1)}>
+            minus 1
           </button>
+          <span>
+            page {page + 1} of {numberPages}
+          </span>
           <button
             className="pageDown"
-            onClick={() => this.fetchMembers(this.state.page - 1)}
+            onClick={() => this.updatePage(page + 1)}
           >
-            page minus one
+            plus 1
           </button>
         </div>
       </div>
